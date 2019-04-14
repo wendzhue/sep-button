79f90a0d1b82d55cb409fa9fb5f644464a84a9c1
Wendell <wendell@Wendells-MacBook-Pro.local>
2019-04-14 13:06:44 +0800

    build: secondary entry

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NzButtonModule } from 'ng-zorro-antd';
+
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NzButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
